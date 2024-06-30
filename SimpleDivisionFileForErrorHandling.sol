637824c8198b36b268ae38d4767e0b38e7d2163e
aayushh14 <121049803+aayushh14@users.noreply.github.com>
2024-06-30 23:22:10 +0530

    Create SimpleDivisionFileForErrorHandling.sol

---
+++ SimpleDivisionFileForErrorHandling.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract SimpleDivision {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function divide(uint256 numerator, uint256 denominator) public view returns (uint256) {
+        require(msg.sender == owner, "You are not the owner");
+
+        require(denominator != 0, "Denominator cannot be zero");
+
+        uint256 result = numerator / denominator;
+
+        assert(result * denominator == numerator);
+        if (denominator < 0) {
+            revert("Denominator cannot be negative");
+        }
+
+        return result;
+    }
+}
